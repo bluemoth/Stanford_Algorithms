bff4c5026ac3d6a1f4fc7c37fc8c7c190488c068
Jacob Case <jcase.bus@gmail.com>
2022-02-14 11:06:36 -0700

    cleaned up SCC file, added bug list for iterative and recursive approaches

--- Module2/SCC/StrongComputedComponents.py
+++ Module2/SCC/StrongComputedComponents.py
@@ -1,7 +1,15 @@
 '''
 Algorithm for computing strongly connected components (Kosaraju's Two-Pass Algorithm) - Stanford/Coursera
-'''
 
+14Feb2021
+Bugs:
+-Iterative approach works for discovering all nodes, however computing the correct finishing time log doesn't compute correctly for test7 and test9
+    -This is because during exploration with edges reversed, new nodes pushed to the stack in wrong order
+    -A method to resolve this may be to log the parent node and consider that during backtracking
+
+-Recursive approach passes all files but test9
+'''
+import time
 
 def build_graph(filename):
 	graph = dict()
@@ -18,48 +26,43 @@ def build_graph(filename):
 	return graph
 
 # DFS top loop for computing SCCs
-def DFS_Loop(Graph, times=0):
-    global finish_time_log 
-    global finish_time
-    global s
-    global explored_list
-    global stack
-    global leader
-    global current_node_count
+def DFS_Loop(Graph, times=0, recursive=False):
+    global finish_time_log, finish_time, s, explored_list, stack, leader, current_node_count
     # leader variable relevant on seond pass
     s = None
     finish_time_log = {}
     explored_list = [False] * maxNodeCount
-    current_node_count = []
+    current_node_count = 0
     finish_time = 0
+    stack = []
 
     if times == 0:
         for i in Graph:
             # for each node within list, if not explored, set leader to that node
             if explored_list[i] == False:
                 s = i
-                DFS_recursive(Graph,i)
+                if recursive == True:
+                    DFS_recursive(Graph,i)
+                else:
+                    DFS_iterative(Graph,i)
     else:
         for i in times:
             if explored_list[i] == False:
                 s = i
-                DFS_recursive(Graph,i)
+                if recursive == True:
+                    DFS_recursive(Graph,i)
+                else:
+                    DFS_iterative(Graph,i)
             if s not in leader:
-                leader[s] = len(current_node_count)
-                current_node_count = []
+                leader[s] = current_node_count
+                current_node_count = 0
 
 # recursive setup of DFS
 def DFS_recursive(Graph, vertex):
-    global finish_time_log 
-    global finish_time
-    global s
-    global explored_list
-    global stack
-    global leader
-    global current_node_count
+    global finish_time_log, finish_time, s, explored_list, stack, leader, current_node_count
 
-    explored_list.append(vertex)
-    current_node_count.append(vertex)
+    explored_list[vertex] = True
+    current_node_count += 1
     if vertex in Graph:
         for edge in Graph[vertex]:
             if explored_list[edge] == False:
@@ -67,47 +70,9 @@ def DFS_recursive(Graph, vertex):
         finish_time += 1
         finish_time_log[edge] = finish_time
 
-def DFS_Loop_Iter(Graph, times=0):
-    global finish_time_log 
-    global finish_time
-    global s
-    global explored_list
-    global stack
-    global leader
-    global current_node_count
-    # leader variable relevant on seond pass
-    s = None
-    finish_time = 0
-    finish_time_log = {}
-    explored_list = [False] * maxNodeCount
-    current_node_count = 0
-
-    if times == 0:
-        for i in Graph:
-            current_node_count = 0 
-            # for each node within list, if not explored, set leader to that node
-            if explored_list[i] == False:
-                s = i
-                DFS_iterative(Graph,i)
-    else:
-        for i in times:
-            if explored_list[i] == False:
-                s = i
-                DFS_iterative(Graph,i)
-            if s not in leader:
-                if current_node_count == 0:
-                    current_node_count = 1
-                leader[s] = current_node_count
-                current_node_count = 0
-
+# iterative DFS approach using stack
 def DFS_iterative(Graph, vertex):
-    global finish_time_log 
-    global finish_time
-    global s
-    global explored_list
-    global stack
-    global leader
-    global current_node_count
+    global finish_time_log, finish_time, s, explored_list, stack, leader, current_node_count
 
     stack = [vertex]
     while stack:
@@ -118,7 +83,8 @@ def DFS_iterative(Graph, vertex):
                 current_node_count += 1
                 for w in Graph[v]:
                     stack.append(w)
-            else: 
+            # node already explored, log finish time and continue backtrack
+            else:
                 if v not in finish_time_log:
                     finish_time +=1 
                     finish_time_log[v] = finish_time
@@ -129,7 +95,7 @@ def DFS_iterative(Graph, vertex):
             finish_time_log[v] = finish_time
             stack.pop()
             
-def transposeGraph(Graph):
+def transpose_graph(Graph):
     edge_list = set()
     new_graph = dict()
     for node, edges in Graph.items():
@@ -143,73 +109,49 @@ def transposeGraph(Graph):
 
 def main():
     # file names
-    test1File = "Module2/SCC/input_mostlyCycles_10_32.txt" # should return 11, 10, 5, 4, 1
-    test2File = "Module2/SCC/input_mostlyCycles_20_128.txt" # should return 61, 46, 15, 3, 2
-    test3File = "/Users/jacobcase/Documents/Projects/Workspace/Stanford_Algorithms/Module2/SCC/input_mostlyCycles_1_8.txt" # should return 4, 2, 2, 0, 0
-    test4File = "Module2/SCC/input_mostlyCycles_11_32.txt" # should return 16,8,5,2,1
-    test5File = "Module2/SCC/input_mostlyCycles_30_800.txt" # should return 437,256,51,44,10
-    test6File = "Module2/SCC/input_mostlyCycles_68_320000.txt" # 271384,33830,9100,3102,850
-    test7File = "Module2/SCC/Jsemko_1.txt" # 3,3,3,0,0
-    test8File = "Module2/SCC/Jsemko_2.txt" # 6,3,2,1,0    
-    test9File = "Module2/SCC/MB_1.txt" # 3,1,1,0,0  
-    homeworkFile = "/Users/jacobcase/Documents/Projects/Workspace/Stanford_Algorithms/Module2/SCC/scc.txt"
-
-    
+    test1 = "Module2/SCC/input_mostlyCycles_10_32.txt" # should return 11, 10, 5, 4, 1
+    test2 = "Module2/SCC/input_mostlyCycles_20_128.txt" # should return 61, 46, 15, 3, 2
+    test3 = "Module2/SCC/input_mostlyCycles_1_8.txt" # should return 4, 2, 2, 0, 0
+    test4 = "Module2/SCC/input_mostlyCycles_11_32.txt" # should return 16,8,5,2,1
+    test5 = "Module2/SCC/input_mostlyCycles_30_800.txt" # should return 437,256,51,44,10
+    test6 = "Module2/SCC/input_mostlyCycles_68_320000.txt" # 271384,33830,9100,3102,850
+    test7 = "Module2/SCC/Jsemko_1.txt" # 3,3,3,0,0
+    test8 = "Module2/SCC/Jsemko_2.txt" # 6,3,2,1,0    
+    test9 = "Module2/SCC/MB_1.txt" # 3,1,1,0,0  
+    SCCFile = "Module2/SCC/scc.txt"
 
     # globals
-    global finish_time_log 
-    global finish_time
-    global s
-    global explored_list
-    global stack
-    global leader
-    global current_node_count
-    global maxNodeCount
+    global finish_time_log, finish_time, s, explored_list, stack, leader, current_node_count, maxNodeCount
         
-    maxNodeCount = 11 # homework # of nodes 875715
+    maxNodeCount = 875715 # homework # of nodes 875715
     finish_time_log = {}
     leader = {}
-    finish_time = 0
-    s = None
-    explored_list = [False] * maxNodeCount 
-    stack = [] 
 
+    #time tracking
+    start = time.time()
 
-    # setup - construct the graph from file
-    graph = build_graph(filename=test7File)
+    #Step 1: - construct the graph from file
+    graph = build_graph(filename=SCCFile)
 
-    # Step 1: Create graph and reverse graph
-    rev_graph = transposeGraph(graph)
-    # print(rev_graph,"\n")
+    #transpose graph
+    rev_graph = transpose_graph(graph)
 
     # Step 2: run DFS Loop on Grev -> Purpose is to compute ordering of nodes by finishing times (stord in dictionary)
-    # DFS_Loop(graph)
-    # # print(explored_list)
-    # print(finish_time_log) 
+    DFS_Loop(rev_graph)
 
-    DFS_Loop_Iter(rev_graph)
-    # print(explored_list)
-    # print(finish_time_log)
-    
+    #reverse finish time for nodes
     reverse_times = []
     for node in finish_time_log:
         reverse_times.append(node)
     reverse_times.reverse()
-    # print(reverse_times)
+
 
     # # Step 3: run DFS loop again on G -> but this time, the order of the processing needs to be done in computed order, completed above
-    # DFS_Loop(graph, reverse_times)    
-    # # print("\n")
-    # for leader_node, node_count in leader.items():
-    #     print(leader_node, node_count)
-
-    DFS_Loop_Iter(graph, reverse_times)
-    # print("\n")
-    # for leader_node, node_count in leader.items():
-    #     print(leader_node, node_count)
+    DFS_Loop(graph, reverse_times)
+    print("Time to complete Kosaraju: ", time.time()-start)
     scc = sorted(leader.values())
     print(scc[-5:])
-    # 13Feb2022 - above produces correct answer
+
 # ------------------------------- Main File -------------------------------
 
 main()

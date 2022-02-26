d8a8573f498385c28cf5f7f74b2b8c62c97cf804
Jacob Case <jcase.bus@gmail.com>
2022-02-25 22:06:43 -0700

    dijkstra implementation using simple queue'd vertices. Implementation works for h/w assignment.

--- Module2/Dijkstra/Dijkstra.py
+++ Module2/Dijkstra/Dijkstra.py
@@ -9,10 +9,20 @@ The rest of the pairs of this row indicate the other vertices adjacent to vertex
 Your task is to run Dijkstra's shortest-path algorithm on this graph, using 1 (the first vertex) as the source vertex, 
 and to compute the shortest-path distances between 1 and every other vertex of the graph. 
 If there is no path between a vertex vv and vertex 1, we'll define the shortest-path distance between 1 and vv to be 1000000. 
+
+Feb 25 2022 - Implementation below works for assignment (undirected adjacency list). The test cases from github (below) may have issues.
+During test of t1-t3, outputs at times are correct for vertex's of interest. Other times they are way off. 
+For simple test cases (t4), implementation works fine. Running time of this vertex_queue setup is O(V^2)
+
+For another challenge, may look to implement algorithm using a priority queue or heap
+
+https://github.com/bluemoth/stanford-algs/tree/master/testCases/course2/assignment2Dijkstra
+
 '''
 
-
+from cmath import inf
 import time
+
 
 def build_graph(filename):
     with open(filename, 'r') as file:
@@ -28,25 +38,54 @@ def build_graph(filename):
                 graph[vertex][int(edge)] = int(weight)
     return graph
 
-def Dijkstra(graph):
-    None
 
+def Dijkstra(graph, vertex=1):
+    dist = dict()
+    vertex_queue = [vertex]
 
+    for v in graph:
+        dist[v] = inf
+        vertex_queue.append(v)
+    
+    dist[vertex] = 0
 
+    while vertex_queue:
+        vertex_queue.pop(0)
+        for u in vertex_queue:
+            for edge in graph[u]:
+                alt = dist[u] + graph[u][edge]
+                if alt < dist[edge]:
+                    dist[edge] = alt
+    return dist
 
 def main():
 
     # Test / home work data files
-    t1 = ""
-    t2 = ""
-    t3 = ""
-    t4 = ""
+    t1 = "Module2/Dijkstra/input_random_1_4.txt" # expected output 253,172,197,242,331,402,143,272,249,265
+    t2 = "Module2/Dijkstra/input_random_10_16.txt" # expected output 588,405,675,521,909,328,418,957,830,839
+    t3 = "Module2/Dijkstra/input_random_20_64.txt" # expected output 699513,452243,60365,166860,289662,820910,593399,836776,621238,439299
+    t4 = "Module2/Dijkstra/TR_input_1.txt" # 0,1,2,3,4,4,3,2
     t5 = ""
-    homeworkData = "Module2/Dijkstra/Dijkstra.txt"
+    homeworkData = "Module2/Dijkstra/Dijkstra.txt" 
     
+    # test graph for formatting/initial usage
+    g = { 1 : {2:1, 3:4},
+          2 : {4:6, 3:2},
+          3 : {4:3},
+          4 : {}
+    }
+    start = time.time()
+    # for homework, interested in distances from source vertex 1 to following vertexs 7,37,59,82,99,115,133,165,188,197
+    vertex_of_interest = [7,37,59,82,99,115,133,165,188,197]
+
     # assignments
     graph = dict()
     graph = build_graph(filename=homeworkData)
-    print(graph)
+
+    distances = Dijkstra(graph)
+    print(distances)
+    # for v in vertex_of_interest:
+    #     print(distances[v])
+    print(time.time()-start)
 
 main()

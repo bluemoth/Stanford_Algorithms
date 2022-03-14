d37cc7fac02c5cbdadffb30f4e8d65ac947d8696
Jacob Case <jcase.bus@gmail.com>
2022-03-13 21:14:26 -0700

    added test cases

---
+++ Module2/MedianMaintenance/MedianMaintenance.py
@@ -0,0 +1,37 @@
+'''
+The goal of this problem is to implement the "Median Maintenance" algorithm (covered in the Week 3 lecture on heap applications).  
+The text file contains a list of the integers from 1 to 10000 in unsorted order; you should treat this as a stream of numbers, arriving one by one.  
+Letting Xi denote the ith number of the file, the kth median Mk is defined as the median of the numbers x1....xk.
+
+
+Find the sum of these 10000 medians, modulo 10000 (i.e., only the last 4 digits). 
+'''
+
+
+def loadHomework(filename):
+    print(filename)
+
+
+
+t1 = "Module2/MedianMaintenance/test1.txt" # 142
+t2 = "Module2/MedianMaintenance/test2.txt" # 9335
+t3 = "Module2/MedianMaintenance/input_random_10_40.txt" # 695
+t4 = ""
+t5 = ""
+
+homeworkFile = "Module2/MedianMaintenance/Median.txt"
+
+
+def main():
+    loadHomework(t1)
+
+
+
+
+
+
+
+
+
+
+main()

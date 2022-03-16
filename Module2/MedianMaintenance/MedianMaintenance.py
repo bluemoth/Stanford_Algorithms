54c083128416da84d6061df2a722e0b53e7474d4
Jacob Case <jcase.bus@gmail.com>
2022-03-16 10:29:57 -0700

    Changed routine so that heap_low manages collection of negative values so heapq operations work for pushing/extractMin()

--- Module2/MedianMaintenance/MedianMaintenance.py
+++ Module2/MedianMaintenance/MedianMaintenance.py
@@ -5,6 +5,10 @@ Letting Xi denote the ith number of the file, the kth median Mk is defined as th
 
 
 Find the sum of these 10000 medians, modulo 10000 (i.e., only the last 4 digits). 
+
+Important to handle all operations within O(log n) time (i.e heap operations)
+Can easily incorporate a linear O(n) runtime routine to calculate median but avoid here
+
 '''
 
 import heapq
@@ -24,29 +28,43 @@ def median_maintenance_algorithm(filename):
 
     # holder for current calculated median val
     median_list = []
-    runningSum = 0
-    heap_low = []
-    heap_high = []
+    runningSum = 0 
+    heap_low = [] # will be a collection of negative numbers since heapq can only support min heap types/operations
+    heap_high = [] # will be a normal collection of numbers
     low_Size = 0
     high_Size = 0
 
     # file loading
     with open(filename, 'r') as file:
         for line in file:
-            # grab each number converting to int for processing
+            # grab each number converting to int for processing...
             num = int(line)
 
-            # Grabbing items one-by-one
+            # first case, if heap_low is empty insert first element there
             if low_Size == 0:
-                # Low heap empty, place first value in there. Update size, sum, and medians list for tracking
-                heapq.heappush(heap_low, num)
+                heapq.heappush(heap_low, -num)
                 low_Size += 1
-                runningSum += num
-                median_list.append(num)
+            elif low_Size > high_Size:
+                if num >= -heap_low[0]:
+                    # case of inbalance, check new median; if greater put in high
+                    heapq.heappush(heap_high, num)
+                else:
+                    # is inbalanced, but new number needs to go low; transfer top value from low to high
+                    heapq.heappush(heap_high, -(heapq.heappop(heap_low)))
+                    heapq.heappush(heap_low, -num)
+                high_Size += 1
+            elif low_Size == high_Size:
+                #no inbalance, can put in either
+                if num > -heap_low[0]:
+                    heapq.heappush(heap_low, -(heapq.heappop(heap_high)))
+                    heapq.heappush(heap_high, num)
+                else:
+                    heapq.heappush(heap_low, -num)
+                low_Size += 1
+
             
-            # Check new incoming value against root heap_low
             
-    
+
     print(heap_low, heap_high)
 
             
@@ -56,6 +74,10 @@ def main():
     start = time.time()
 
     median_maintenance_algorithm(t3)
+
+
+
+
 
     print("elapsed time: ", time.time() - start)
 
